304f188351e3a66f107de4650cbfde74e5e68244
George Markham <georgemarkham@Georges-MacBook-Air.local>
2018-02-15 09:57:19 +0000

    Added Device plugin

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -7,12 +7,21 @@ module.exports = [
     "clobbers": [
       "window.StatusBar"
     ]
+  },
+  {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-  "cordova-plugin-statusbar": "2.4.1"
+  "cordova-plugin-statusbar": "2.4.1",
+  "cordova-plugin-device": "2.0.1"
 };
 // BOTTOM OF METADATA
 });

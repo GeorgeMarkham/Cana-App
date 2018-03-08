d223a28c35d0181f8f10b3869bb45f8212106d15
George Markham <georgemarkham@Georges-MacBook-Air.local>
2018-03-08 11:38:32 +0000

    signup works

--- www/js/app.js
+++ www/js/app.js
@@ -6,8 +6,24 @@ function deviceReady(){
 
     $('#login_btn').on('click', (e)=>{
         e.preventDefault();
+    })
 
-        $.post()
+    $('#signup_btn').on('click', (e)=>{
+        e.preventDefault();
 
+        var email_input = $('#signup_email_input');
+        var password_input = $('#signup_password_input');
+        
+        var data = {
+            email: email_input.val(),
+            password: password_input.val()
+        }
+
+        $.post('http://localhost:8080/signup', data, (data, status)=>{
+            if(status == 'success' && data.success == true){
+                var token = data.token;
+            }
+            
+        })
     })
 }

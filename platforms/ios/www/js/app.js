b2d2b3e7ce7230f4627350ed6e166fc65e82a616
George Markham <georgemarkham@Georges-MacBook-Air.local>
2018-03-13 19:15:49 +0000

    added sqlite plugin

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -21,14 +21,54 @@ function deviceReady(){
 
         $.post('http://localhost:8080/signup', data, (data, status)=>{
             if(status == 'success' && data.success == true){
-                var db = window.sqlitePlugin.openDatabase({name:"user.db"});
+                var token = data.token;
+                var secret = '7raputr5pU2rERudaCRASpUphUguPewR3uTrachaqaswEnapheZUPradRespedruspuchu8ruphayachuxa2Akacuzejezut3unAf5marE2usTewuDEgedeBrAg6dR2q';
+                var db = window.sqlitePlugin.openDatabase({name:"hereby.db", location: 2, createFromLocation: 1});
+                
+                if(KJUR.jws.JWS.verifyJWT(token, secret, {alg: ['HS256']})){
+                    var headerObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(token.split(".")[0]));
+                    var payloadObj = KJUR.jws.JWS.readSafeJSONString(b64utoutf8(token.split(".")[1]));
 
-                db.transaction((tx)=>{
-                    /* 1. Drop users table if it exists*/
-                    /* 2. Create users table if it doesn't exist */
-                    /* 3. Insert token, username and friends list into database */
-                    /* 4. Check and make sure data is stored correctly in the database */
-                })
+                    console.log(headerObj);
+                    var id = payloadObj.id;
+                    var friends = payloadObj.friends;
+
+                    db.transaction((transaction)=>{
+                        /* 1. Drop users table if it exists*/
+                        /* 2. Create users table if it doesn't exist */
+                        /* 3. Insert token, username and friends list into database */
+                        /* 4. Check and make sure data is stored correctly in the database */
+                        var isErr = false;
+                        transaction.executeSql("CREATE TABLE IF NOT EXISTS users (id integer primary key, token text, friends text)", [], (tx, res)=>{
+                            console.log(tx);
+                            console.log(res);
+                        }, 
+                        (err)=>{
+                            isErr = true;
+                            console.log(err);
+                        });
+                        if(!isErr){
+                            transaction.executeSql("INSERT INTO users (id, token, friends) VALUES (?,?,?)", [id, token, friends], (tx, res)=>{
+                                console.log(tx);
+                                console.log(res);
+                            }, 
+                            (err)=>{
+                                isErr = true;
+                                console.log(err);
+                            });
+                        }
+                        if(!isErr){
+                            transaction.executeSql("SELECT * FROM users", [], (tx, res)=>{
+                                console.log(tx);
+                                console.log(res);
+                            }, 
+                            (err)=>{
+                                isErr = true;
+                                console.log(err);
+                            });
+                        }
+                    })
+                }
             }
         })
     })

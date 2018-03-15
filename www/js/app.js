db23c321e7a4a8dab887b8cc8bca63b36fbe7400
George Markham <georgemarkham@Georges-MacBook-Air.local>
2018-03-15 10:24:28 +0000

    saved user to local storage

--- www/js/app.js
+++ www/js/app.js
@@ -32,44 +32,64 @@ function deviceReady(){
                     console.log(headerObj);
                     var id = payloadObj.id;
                     var friends = payloadObj.friends;
+                    
+                    user = {
+                        id: id,
+                        friends: friends,
+                        token: token
+                    }
+                    localStorage.setItem('user', JSON.stringify(user));
 
-                    db.transaction((transaction)=>{
-                        /* 1. Drop users table if it exists*/
-                        /* 2. Create users table if it doesn't exist */
-                        /* 3. Insert token, username and friends list into database */
-                        /* 4. Check and make sure data is stored correctly in the database */
-                        var isErr = false;
-                        transaction.executeSql("CREATE TABLE IF NOT EXISTS users (id integer primary key, token text, friends text)", [], (tx, res)=>{
-                            console.log(tx);
-                            console.log(res);
-                        }, 
-                        (err)=>{
-                            isErr = true;
-                            console.log(err);
-                        });
-                        if(!isErr){
-                            transaction.executeSql("INSERT INTO users (id, token, friends) VALUES (?,?,?)", [id, token, friends], (tx, res)=>{
-                                console.log(tx);
-                                console.log(res);
-                            }, 
-                            (err)=>{
-                                isErr = true;
-                                console.log(err);
-                            });
-                        }
-                        if(!isErr){
-                            transaction.executeSql("SELECT * FROM users", [], (tx, res)=>{
-                                console.log(tx);
-                                console.log(res);
-                            }, 
-                            (err)=>{
-                                isErr = true;
-                                console.log(err);
-                            });
-                        }
-                    })
+                    if(localStorage.getItem('user') != null){
+                        console.log(localStorage.getItem('user'));
+
+                    } else {
+                        console.log("Something bad happened")
+                    }
                 }
             }
         })
     })
 }
+
+
+
+/* SQLITE STUFF */
+
+// db.transaction((transaction)=>{
+    //     alert("DB TRANSACTION");
+    //     /* 1. Drop users table if it exists*/
+    //     /* 2. Create users table if it doesn't exist */
+    //     /* 3. Insert token, username and friends list into database */
+    //     /* 4. Check and make sure data is stored correctly in the database */
+    //     var isErr = false;
+    //     transaction.executeSql("CREATE TABLE IF NOT EXISTS users (id integer primary key, token text, friends text)", [], (tx, res)=>{
+    //         console.log(tx);
+    //         console.log(res);
+    //     }, 
+    //     (err)=>{
+    //         isErr = true;
+    //         console.log(err);
+    //     });
+    //     if(!isErr){
+    //         transaction.executeSql("INSERT INTO users (id, token, friends) VALUES (?,?,?)", [id, token, friends], (tx, res)=>{
+    //             console.log(tx);
+    //             console.log(res);
+    //         }, 
+    //         (err)=>{
+    //             isErr = true;
+    //             console.log(err);
+    //         });
+    //     }
+    //     if(!isErr){
+    //         transaction.executeSql("SELECT * FROM users", [], (tx, res)=>{
+    //             console.log(tx);
+    //             console.log(res);
+    //             alert(res);
+    //         }, 
+    //         (err)=>{
+    //             isErr = true;
+    //             console.log(err);
+    //         });
+    //     }
+    // })
